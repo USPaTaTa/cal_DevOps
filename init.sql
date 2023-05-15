3b65a32600c3ef43f59beeeaeff940109f907efd
Clément <clement.metois@selection-enr.com>
2023-05-15 11:56:25 +0200

    dgerg

--- init.sql
+++ init.sql
@@ -1,6 +1,6 @@
 USE `bdd_fatsecret`;
 
-CREATE TABLE fatsecret (
+CREATE TABLE fatsecret(
   idfatsecret int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   time varchar(255) NOT NULL,
   nom varchar(255) NOT NULL,
@@ -11,5 +11,5 @@ CREATE TABLE fatsecret (
   imc varchar(255) NOT NULL,
   objectifpoids varchar(255) NOT NULL,
   objectifprecedent varchar(255) NOT NULL,
-  interpretation_IMC varchar(255) NOT NULL,
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  interpretation_IMC varchar(255) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
