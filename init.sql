18f708bce31333699f03beb289ebc198127bfbf3
Clément <clement.metois@selection-enr.com>
2023-05-15 11:30:34 +0200

    rgfbgf

--- init.sql
+++ init.sql
@@ -12,4 +12,4 @@ CREATE TABLE fatsecret (
   objectifpoids varchar(255) NOT NULL,
   objectifprecedent varchar(255) NOT NULL,
   interpretation_IMC varchar(255) NOT NULL,
-)ENGINE='InnoDB' DEFAULT CHARSET='utf8mb4' COLLATE='utf8mb4_0900_ai_ci';
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
